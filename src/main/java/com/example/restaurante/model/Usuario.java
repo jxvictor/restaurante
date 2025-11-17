a1323b49d6a57753950797510c876fb1fb921e9e
jxvictor <jvalves2018@gmail.com>
2025-11-17 13:49:27 -0300

    Subindo usuário

--- src/main/java/com/example/restaurante/model/Usuario.java
+++ src/main/java/com/example/restaurante/model/Usuario.java
@@ -8,8 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
-public class Usuario {
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String username;
